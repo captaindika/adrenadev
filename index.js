ec725e404f0df0b64014bb00259e4c2db7df261b
Dika <dika.frandita@gmail.com>
2020-11-11 18:05:33 +0700

    add app.use(json)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const {login} = require('./app/controller/user')
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.use(cors())
 
 const userRoute = require('./app/routes/user')
